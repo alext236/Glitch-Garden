89ea9d6c752b0f437572927afdef29812ae0600b
alext236 <alex.tuandao@gmail.com>
2015-07-29 22:51:20 +0300

    Initiate Project

---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public bool autoLoadNextLevel;
+    public float loadNextLevelAfter;
+
+    void Start() {
+        if (autoLoadNextLevel) {
+            Invoke("LoadNextLevel", loadNextLevelAfter);
+        }        
+    }
+
+    public void LoadLevel(string name) {
+        Debug.Log("Level load requested for " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitRequest() {
+        Debug.Log("Quit the game");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel() {
+        Application.LoadLevel(Application.loadedLevel + 1);
+    }
+
+
+}
