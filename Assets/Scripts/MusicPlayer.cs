2e418cc11be84a83993475686a987a1bf61ec684
alext236 <alex.tuandao@gmail.com>
2015-08-01 22:52:21 +0300

    Music Player redone
    Create a singleton music player on Splash screen (since we don't ever go
    back to splash)

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 
-    private static MusicPlayer instance = null;
+    
     private AudioSource soundTrack;
 
     public AudioClip startMenu;
@@ -11,38 +11,24 @@ public class MusicPlayer : MonoBehaviour {
     public AudioClip gameOver;
 
     void Awake() {
-        if (instance != null) {
-            GameObject.Destroy(gameObject);
-            Debug.Log("A duplicate music player is destroyed.");
-        }
-        else {
-            instance = this;
-            GameObject.DontDestroyOnLoad(gameObject);
-            soundTrack = GetComponent<AudioSource>();
-        }
+        soundTrack = GetComponent<AudioSource>();
+        DontDestroyOnLoad(this.gameObject);
     }
 
     public void Start() {        
-        soundTrack.clip = startMenu;
-        soundTrack.loop = true;
-        soundTrack.Play();
     }
-     
+
 
     public void OnLevelWasLoaded(int level) {
-        Debug.Log("Set music");        
-        if (level == 0) {            //Start Menu and Tutorial
+        Debug.Log("Set music");
 
-            //soundTrack.clip = startMenu;
-            //soundTrack.Play();
-        }
-        else if (level == 1) {      //Main Game
-            
-            soundTrack.clip = mainGame;
+        if (level == 1) {      //Main Game
+
+            soundTrack.clip = startMenu;
             soundTrack.Play();
         }
         else if (level == 2) {      //Game over
-            
+
             soundTrack.clip = gameOver;
             soundTrack.Play();
         }
