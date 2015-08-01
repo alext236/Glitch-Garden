051b168c5b1741f18e6083007bece2b3a2ae852f
alext236 <alex.tuandao@gmail.com>
2015-08-01 23:13:58 +0300

    Add scenes
    Add options, win, lose, first level screen with navigational buttons

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -15,25 +15,25 @@ public class MusicPlayer : MonoBehaviour {
         DontDestroyOnLoad(this.gameObject);
     }
 
-    public void Start() {        
-    }
-
-
     public void OnLevelWasLoaded(int level) {
         Debug.Log("Set music");
 
-        if (level == 1) {      //Main Game
-
-            soundTrack.clip = startMenu;
+        if (level == 1) {       //Start Menu
+            if (soundTrack.clip != startMenu) {
+                soundTrack.clip = startMenu;
+                soundTrack.Play();
+            }            
+        }
+        else if (level == 2) {  //Options
+            return;
+        }
+        else if (level == 3) {  //Main game
+            soundTrack.clip = mainGame;
             soundTrack.Play();
         }
-        else if (level == 2) {      //Game over
-
+        else if (level == 4 || level == 5) {
             soundTrack.clip = gameOver;
             soundTrack.Play();
-        }
-        else if (level == 3) {
-            return;
         }
     }
 }
