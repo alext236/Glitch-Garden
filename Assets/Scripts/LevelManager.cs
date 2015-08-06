cb1cd86bbb8cbb7a207d81f5941d3c2e33de4e9a
alext236 <alex.tuandao@gmail.com>
2015-08-06 22:48:54 +0300

    Add Start Menu fade in

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -12,7 +12,7 @@ public class LevelManager : MonoBehaviour {
     }
 
     public void LoadLevel(string name) {
-        Debug.Log("Level load requested for " + name);
+        Debug.Log("Level load requested for " + name);        
         Application.LoadLevel(name);
     }
 
