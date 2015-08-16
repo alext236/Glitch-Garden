a07db3c3fb961730ac56d8326f86a28c09c9e95f
alext236 <alex.tuandao@gmail.com>
2015-08-17 00:07:14 +0300

    Fix various animations
    - Add npc_fox and its animations
    - Add animation events to set speed for attackers in different animation
    states

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -6,22 +6,37 @@ public class Attacker : MonoBehaviour {
     [Range(-1f, 2f)]
     public float walkSpeed;
 
+    private float currentSpeed;
+
     private Animator animator;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         animator = GetComponent<Animator>();
 
         Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
         myRigidBody.isKinematic = true;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //Move left when animation state is "Walk"
-        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk")) {
-            transform.Translate(Vector3.left * Time.deltaTime * walkSpeed, Space.Self);
-        }
-        
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //Movement is controlled with animation events (by setting speed = 0 when not moving)
+        transform.Translate(Vector3.left * Time.deltaTime * currentSpeed, Space.Self);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision) {
+        Debug.Log(name + " trigger enter");
+    }
+
+    public void SetCurrentSpeed(float speed) {
+        currentSpeed = speed;
+    }
+    
+    public void ResetOriginalSpeed() {
+        currentSpeed = walkSpeed;
+    }
+
+    public void StrikeCurrentTarget(float damage) {
+        Debug.Log("This attacker is attacking with " + damage + " damage");
+    }
 }
