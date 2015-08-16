8ec5bfec0c4ad86ad758f70ddc4f947e97c97aa7
alext236 <alex.tuandao@gmail.com>
2015-08-16 18:25:59 +0300

    Add Attacker script
    - Attacker.cs
    - Move the attackers with script
    - Add Box Collider 2D for the attackers

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(-1f, 2f)]
+    public float walkSpeed;
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Move left when animation state is "Walk"
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Walk")) {
+            transform.Translate(Vector3.left * Time.deltaTime * walkSpeed, Space.Self);
+        }
+        
+	}
+}
