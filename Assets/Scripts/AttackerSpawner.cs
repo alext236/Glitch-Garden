89a421b34e5f66098877483fdfaef7cfcf483418
alext236 <alex.tuandao@gmail.com>
2015-08-27 01:10:33 +0300

    Start on AttackerSpawner.cs
    - Now a specified attacker spawn for predetermined copies, with a
    predetermined period (in sec) in between

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackerSpawner : MonoBehaviour {
+
+    public GameObject[] attackerList;
+    //Think about whether to use array or specific GameObject
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(SpawnAttacker(attackerList[0], 4));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator SpawnAttacker(GameObject attacker, int amount) {
+        if (!attacker) {
+            Debug.LogError("Attacker not defined!");
+        }
+        //think if there's a way to calculate the x spawn position based on width of play space
+        for (int i = 0; i < amount; i++) {
+            Vector3 spawnPos = new Vector3(9f, Random.Range(1, 6));
+            GameObject newAtk = Instantiate(attacker, spawnPos, Quaternion.identity) as GameObject;
+            newAtk.transform.SetParent(transform);
+
+            yield return new WaitForSeconds(3);
+        }
+
+        
+
+    }
+}
